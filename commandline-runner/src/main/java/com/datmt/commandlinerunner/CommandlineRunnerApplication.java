e2f54e2350e34b836de488a293931a856eec3384
Mạnh Đạt <dat.tm24@gmail.com>
2022-11-29 10:53:13 +0700

    create multiple beans for commandlinerunner

--- commandline-runner/src/main/java/com/datmt/commandlinerunner/CommandlineRunnerApplication.java
+++ commandline-runner/src/main/java/com/datmt/commandlinerunner/CommandlineRunnerApplication.java
@@ -6,18 +6,27 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
-public class CommandlineRunnerApplication implements CommandLineRunner {
+public class CommandlineRunnerApplication {
 
    Logger logger = LoggerFactory.getLogger(CommandlineRunnerApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(CommandlineRunnerApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-       logger.info("Hello, world! From commandline runner");
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            logger.info("Hello, world! From commandline runner as a bean");
+        };
     }
 
+    @Bean
+    CommandLineRunner runner2() {
+        return args -> {
+            logger.info("Hello, world! From commandline runner ALSO as a bean");
+        };
+    }
 }
