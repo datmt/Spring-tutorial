5feadba6c2166db07871883dff776c7009c2c1c6
Mạnh Đạt <dat.tm24@gmail.com>
2022-11-29 11:15:27 +0700

    remove beans in main class

--- commandline-runner/src/main/java/com/datmt/commandlinerunner/CommandlineRunnerApplication.java
+++ commandline-runner/src/main/java/com/datmt/commandlinerunner/CommandlineRunnerApplication.java
@@ -1,30 +1,13 @@
 package com.datmt.commandlinerunner;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class CommandlineRunnerApplication {
 
-   Logger logger = LoggerFactory.getLogger(CommandlineRunnerApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(CommandlineRunnerApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            logger.info("Hello, world! From commandline runner as a bean");
-        };
-    }
-
-    @Bean
-    CommandLineRunner runner2(DummyService dummyService) {
-        return args -> dummyService.doSomething();
-    }
 }
