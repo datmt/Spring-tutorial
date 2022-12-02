7a6b59d4d526faa8893bc6bbb8e99f2b638fd5d7
Mạnh Đạt <dat.tm24@gmail.com>
2022-12-02 23:37:27 +0700

    add test folder

---
+++ spring-logging-elasticsearch/src/test/java/com/datmt/spring/springloggingelasticsearch/SpringLoggingElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datmt.spring.springloggingelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
