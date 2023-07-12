50eb399a0902ed81306c3e78abafd2da80511efa
Mạnh Đạt <dat.tm24@gmail.com>
2023-07-12 15:41:09 +0700

    add car model

--- spring-junit5-mocktio/src/main/java/com/damt/test/springjunit5mockito/models/Car.java
+++ spring-junit5-mocktio/src/main/java/com/damt/test/springjunit5mockito/models/Car.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Entity
+@AllArgsConstructor
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
