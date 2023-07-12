79b4e3ad506c7c67fb28151fe4c1b98b68df680c
Mạnh Đạt <dat.tm24@gmail.com>
2023-07-12 14:35:38 +0700

    create car crud

---
+++ spring-junit5-mocktio/src/main/java/com/damt/test/springjunit5mockito/controllers/CarController.java
@@ -0,0 +1,34 @@
+package com.damt.test.springjunit5mockito.controllers;
+
+import com.damt.test.springjunit5mockito.models.Car;
+import com.damt.test.springjunit5mockito.services.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+public class CarController {
+    private final CarService carService;
+
+    @PostMapping("/")
+    public Car create(@RequestBody Car car) {
+        return carService.create(car);
+    }
+
+    @GetMapping("/{id}")
+    public Car find(@PathVariable Long id) {
+        return carService.findById(id).orElseThrow(() -> new RuntimeException("Car not found"));
+    }
+
+    @PutMapping("/")
+    public Car update(@RequestBody Car car) {
+        return carService.update(car);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        carService.delete(id);
+        return "Car deleted";
+    }
+}
--- spring-junit5-mocktio/src/main/java/com/damt/test/springjunit5mockito/services/CarService.java
+++ spring-junit5-mocktio/src/main/java/com/damt/test/springjunit5mockito/services/CarService.java
@@ -5,6 +5,8 @@ import com.damt.test.springjunit5mockito.repositories.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CarService {
@@ -16,24 +18,13 @@ public class CarService {
         if (car.getId() != null) {
             throw new RuntimeException("Id must be null");
         }
-        if (car.getName() == null) {
-            throw new RuntimeException("Name must not be null");
-        }
-        if (car.getModel() == null) {
-            throw new RuntimeException("Model must not be null");
-        }
-        if (car.getMaker() == null) {
-            throw new RuntimeException("Maker must not be null");
-        }
-        if (car.getColor() == null) {
-            throw new RuntimeException("Color must not be null");
-        }
+        verifyCar(car);
 
         return carRepository.save(car);
     }
 
-    public Car findById(Long id) {
-        return carRepository.findById(id).orElse(null);
+    public Optional<Car> findById(Long id) {
+        return carRepository.findById(id);
     }
 
     //update car
@@ -42,6 +33,15 @@ public class CarService {
         if (car.getId() == null) {
             throw new RuntimeException("Id must not be null");
         }
+        verifyCar(car);
+
+        //check if car exists
+        carRepository.findById(car.getId()).orElseThrow(() -> new RuntimeException("Car not found"));
+
+        return carRepository.save(car);
+    }
+
+    private void verifyCar(Car car) {
         if (car.getName() == null) {
             throw new RuntimeException("Name must not be null");
         }
@@ -54,10 +54,9 @@ public class CarService {
         if (car.getColor() == null) {
             throw new RuntimeException("Color must not be null");
         }
+    }
 
-        //check if car exists
-        carRepository.findById(car.getId()).orElseThrow(() -> new RuntimeException("Car not found"));
-
-        return carRepository.save(car);
+    public void delete(Long id) {
+        carRepository.deleteById(id);
     }
 }
