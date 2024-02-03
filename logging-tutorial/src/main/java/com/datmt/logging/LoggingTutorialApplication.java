5a916a52be982f97b0f254011e0176eadd02e6a6
Mạnh Đạt <dat.tm24@gmail.com>
2024-02-03 13:51:39 +0700

    start loggin tutorial

---
+++ logging-tutorial/src/main/java/com/datmt/logging/LoggingTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.datmt.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingTutorialApplication.class, args);
+	}
+
+}
---
+++ logging-tutorial/src/test/java/com/datmt/logging/LoggingTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datmt.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
