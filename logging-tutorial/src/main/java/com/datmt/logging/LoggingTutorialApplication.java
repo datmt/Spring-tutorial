d813572c4329e677c4e4a8ab276bf5c2f3bf2314
Mạnh Đạt <dat.tm24@gmail.com>
2024-02-04 07:00:15 +0700

    add logback

--- logging-tutorial/src/main/java/com/datmt/logging/LoggingTutorialApplication.java
+++ logging-tutorial/src/main/java/com/datmt/logging/LoggingTutorialApplication.java
@@ -2,12 +2,31 @@ package com.datmt.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+
+@Slf4j
 public class LoggingTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoggingTutorialApplication.class, args);
 	}
 
+	@Slf4j
+	@Component
+	public static class TestLogService {
+		public TestLogService() {
+			while (true) {
+				log.trace("Hello World");
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
