e4c7644e20c8a692b93e74c10729228049293bc7
datmt <dat.tm24@gmail.com>
2024-04-08 22:30:55 +0700

    remove redundancy

--- spring-data-postgres/src/main/java/com/datmt/springdatapostgres/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.datmt.springdatapostgres.repository;
-
-import com.datmt.springdatapostgres.model.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
