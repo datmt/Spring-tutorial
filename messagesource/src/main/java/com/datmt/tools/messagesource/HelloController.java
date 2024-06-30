af9a3a09da3b2edb9980a2ebabf6759eae0e9fb6
datmt <dat.tm24@gmail.com>
2024-06-30 11:39:13 +0700

    complete example

--- messagesource/src/main/java/com/datmt/tools/messagesource/HelloController.java
+++ messagesource/src/main/java/com/datmt/tools/messagesource/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.Locale;
 
@@ -23,5 +22,37 @@ public class HelloController {
     @GetMapping
     public String hello(Locale locale) {
         return messageSource.getMessage("greeting", null, locale);
+
+        //Default message
+//        return messageSource.getMessage("non.existent.key", null, "Default Message", locale);
+
+
+        //Parameters
+//        return messageSource.getMessage("welcome.user", new Object[]{"John"}, locale);
+//        return messageSource.getMessage("order.status", new Object[]{"12345", "shipped"}, locale);
+
+
+        //Multiple lines
+//        return messageSource.getMessage("text.multiple.lines", null, locale);
+
+
+        //date and number
+        /*
+        String greeting = messageSource.getMessage("greeting", null, locale);
+        String dateFormatPattern = messageSource.getMessage("date.format", null, locale);
+        String numberFormatPattern = messageSource.getMessage("number.format", null, locale);
+
+        // Formatting date
+        DateFormat dateFormat = new SimpleDateFormat(dateFormatPattern, locale);
+        String formattedDate = dateFormat.format(new Date());
+
+        // Formatting number
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(locale);
+        DecimalFormat numberFormat = new DecimalFormat(numberFormatPattern, symbols);
+
+        String formattedNumber = numberFormat.format(12345.6789);
+        return String.format("%s\nDate: %s\nNumber: %s", greeting, formattedDate, formattedNumber);
+*/
+
     }
 }
--- messagesource/src/main/java/com/datmt/tools/messagesource/LocaleConfig.java
+++ messagesource/src/main/java/com/datmt/tools/messagesource/LocaleConfig.java
@@ -9,9 +9,6 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class LocaleConfig implements WebMvcConfigurer {
